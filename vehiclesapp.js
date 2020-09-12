be0831ad0870c3636562c52182a058f62fa0051b
FelipeSchmitt <50593950+FelipeSchmitt@users.noreply.github.com>
2020-09-12 17:01:29 -0300

    Add files via upload

--- vehiclesapp.js
+++ vehiclesapp.js
@@ -18,6 +18,7 @@ var firebaseConfig = {
    vehicle_content.innerHTML=""
     for (doc of Vehicles){
         const box = document.createElement("div")
+        box.id="card"
         box.style.width="95%"
         box.style.margin="auto"
         box.style.height="98%"
@@ -38,19 +39,77 @@ var firebaseConfig = {
         image.src=doc.image
         image.style.width="90%"
         image.style.borderRadius="4px"
+        image.style.borderBottomLeftRadius="0"
+        image.style.borderBottomRightRadius="0"
         image.style.display="block"
         image.style.margin="auto"
         const price = document.createElement("h4")
         price.innerHTML="R$" + doc.price
+        price.style.backgroundColor="#343a40"
+        price.style.color="white"
+        price.style.borderRadius="7px"
+        price.style.borderTopLeftRadius="0"
+        price.style.borderTopRightRadius="0"
+        price.style.textAlign="center"
+        price.style.lineHeight="40px"
+        price.style.height="40px"
         price.style.margin="auto"
         price.style.width="90%"
+        const itensdiv = document.createElement("div")
+        itensdiv.id="km"
+        const versao_carro = document.createElement("div")
+        versao_carro.id="modelo"
+        const boxitem = document.createElement("div")
+        boxitem.id="itens"
+        const km_rodados = document.createElement("img")
+        km_rodados.src="km_icon.png"
+        km_rodados.style.width="30px"
+        km_rodados.style.borderRadius="20em"
+
+        itensdiv.appendChild(km_rodados)
+        const h5 = document.createElement("h5")
+        h5.innerHTML=doc.km_rodados
+        const h5_versao = document.createElement("h5")
+        h5_versao.innerHTML="Versão: " + doc.versao_carro
+        h5.style.paddingLeft="5px"
+
+        const cilindradadiv = document.createElement("div")
+        const cilindrada = document.createElement("img")
+        cilindrada.src="https://icon-library.com/images/motor-icon/motor-icon-7.jpg"
+        cilindrada.style.width="30px"
+        cilindradadiv.style.display="flex"
+        cilindradadiv.appendChild(cilindrada)
+        const h5_cilindrada = document.createElement("h5")
+        h5_cilindrada.innerHTML=doc.cilindrada
+        cilindradadiv.appendChild(h5_cilindrada)
+        
+        const transmissaodiv = document.createElement("div")
+        const transmissao = document.createElement("img")
+        transmissao.src="https://static.thenounproject.com/png/363283-200.png"
+        transmissaodiv.style.display="flex"
+        transmissao.style.width="30px"
+        transmissaodiv.appendChild(transmissao)
+        const h5_transmissao = document.createElement("h5")
+        h5_transmissao.innerHTML=doc.transmissao
+        transmissaodiv.appendChild(h5_transmissao) 
+        transmissaodiv.style.marginTop="8px"
+
         const description = document.createElement("p")
         description.innerHTML=doc.description
         description.style.width="90%"
         description.style.margin="auto"
+        description.style.backgroundColor="white"
+        description.style.borderRadius="7px"
         box.appendChild(title)
         box.appendChild(image)
         box.appendChild(price)
+        box.appendChild(boxitem)
+        boxitem.appendChild(itensdiv)
+        itensdiv.appendChild(h5)
+        boxitem.appendChild(versao_carro)
+        versao_carro.appendChild(h5_versao)
+        boxitem.appendChild(cilindradadiv)
+        boxitem.appendChild(transmissaodiv)
         box.appendChild(description)
         vehicle_content.appendChild(box)
 
@@ -59,7 +118,29 @@ var firebaseConfig = {
 
 async function readVehicles() {
     Vehicles = []
-    const logVehicles = await db.collection("vehicles").get()
+    const brand1 = document.getElementById("brand").value
+    const car_name1 = document.getElementById("car_name").value
+    const year1 = document.getElementById("year1").value
+    const year2 = document.getElementById("year2").value
+    var filter_use = ""
+    var sequency = ""
+    if(year1 != "" && year2 != ""){
+      readVehicles_2_sentences()
+    }else
+    if (car_name1 != ""){
+      filter_use = "car_name"
+      sequency = car_name1
+    }else{
+      if(brand1 != ""){
+        filter_use = "brand"
+        sequency = brand1
+    }else{
+      if(brand1 == ""){
+        readVehiclesAll()
+      }
+    }
+    }
+    const logVehicles = await db.collection("vehicles").where(filter_use, "==", sequency).get()
     for (doc of logVehicles.docs) {
     Vehicles.push({
         id: doc.id,
@@ -71,8 +152,63 @@ async function readVehicles() {
         author: doc.data().author,
         image: doc.data().image,
         price: doc.data().price,
+        year: doc.data().year,
+        km_rodados: doc.data(). km_rodados,
+        versao_carro: doc.data(). versao_carro,
+        cilindrada: doc.data().cilindrada,
+        transmissao: doc.data(). transmissao,
         download_link: doc.data().download_link,
         })
     }
     renderVehicles()
 }
+  async function readVehicles_2_sentences(){
+    var sequency = document.getElementById("car_name").value
+    var yearold = document.getElementById("year1").value
+    var yearnew = document.getElementById("year2").value
+    const logVehicles2 = await db.collection("vehicles").where("year", ">=", yearold).get()
+    for (doc of logVehicles2.docs) {
+    Vehicles.push({
+        id: doc.id,
+        date: doc.data().date,
+        title: doc.data().title,
+        description: doc.data().description,
+        brand: doc.data().brand,
+        car_name: doc.data().car_name,
+        author: doc.data().author,
+        image: doc.data().image,
+        price: doc.data().price,
+        year: doc.data().year,
+        km_rodados: doc.data().km_rodados,
+        versao_carro: doc.data(). versao_carro,
+        cilindrada: doc.data().cilindrada,
+        transmissao: doc.data(). transmissao,
+        download_link: doc.data().download_link,
+        })
+    }
+    renderVehicles()
+  }
+
+  async function readVehiclesAll(){
+    const logVehicles3 = await db.collection("vehicles").get()
+    for (doc of logVehicles3.docs) {
+    Vehicles.push({
+        id: doc.id,
+        date: doc.data().date,
+        title: doc.data().title,
+        description: doc.data().description,
+        brand: doc.data().brand,
+        car_name: doc.data().car_name,
+        author: doc.data().author,
+        image: doc.data().image,
+        price: doc.data().price,
+        year: doc.data().year,
+        km_rodados: doc.data().km_rodados,
+        versao_carro: doc.data(). versao_carro,
+        cilindrada: doc.data().cilindrada,
+        transmissao: doc.data(). transmissao,
+        download_link: doc.data().download_link,
+        })
+    }
+    renderVehicles()
+  }
